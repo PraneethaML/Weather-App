b2f3e38b173508af2a54bbc0b0dc144d0b5b412b
PraneethaML <praneetha1993@gmail.com>
2023-11-26 20:26:24 +0530

    refactor: change get_temperature route to GET method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'current_weather#index'
-  post '/current_weather/get_temperature', to: 'current_weather#get_temperature', as: :get_temperature
+  get '/current_weather/get_temperature', to: 'current_weather#get_temperature', as: :get_temperature
 end
