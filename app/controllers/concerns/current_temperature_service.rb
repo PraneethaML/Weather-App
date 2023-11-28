e00c8f2f2dc058e5885adeab4e3ec667d57aafb2
PraneethaML <praneetha1993@gmail.com>
2023-11-28 15:51:03 +0530

    Fix undefined response error

--- app/controllers/concerns/current_temperature_service.rb
+++ app/controllers/concerns/current_temperature_service.rb
@@ -31,7 +31,6 @@ module CurrentTemperatureService
   # Retrieve the current temperature based on the provided zipcode and country parameters using external api
   def fetch_temperature_from_api
     response = { temperature: nil, error: nil, status: nil }
-    status_code = 200
   
     begin
       data = OpenWeatherConfig.client.current_zip(@zipcode, @country)
@@ -51,19 +50,21 @@ module CurrentTemperatureService
                         "Too Many Requests - Key quota exceeded"
                       end
       response[:error] = "#{error_message}. Status: #{status_code}"
+      response[:status] = e.response[:body]['cod'] || 503
     rescue Faraday::ConnectionFailed => e
       puts "Handling connection failure: #{e.response}"
       response[:error] = 'Connection Failed'
+      response[:status] = e.response[:body]['cod'] || 503
     rescue StandardError => e
       # TODO: Invalid api key is also giving in standard error
       puts "Handling standard error: #{e.response}"
       response[:error] = e.message
+      response[:status] = e.response[:body]['cod'] || 503
     ensure
-      status_code = e.response[:body]['cod'] || 503
-      response[:status] = status_code
       puts "Response after external api is #{response}"
       return response  
     end
+    
   end    
 end
   
