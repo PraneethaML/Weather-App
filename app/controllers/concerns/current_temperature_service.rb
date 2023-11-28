7cff5cc0923ed0c96dd8d33f9ad25eac135d02d4
PraneethaML <praneetha1993@gmail.com>
2023-11-28 16:32:05 +0530

    fix error in rescue block

--- app/controllers/concerns/current_temperature_service.rb
+++ app/controllers/concerns/current_temperature_service.rb
@@ -30,8 +30,7 @@ module CurrentTemperatureService
 
   # Retrieve the current temperature based on the provided zipcode and country parameters using external api
   def fetch_temperature_from_api
-    response = { temperature: nil, error: nil, status: nil }
-  
+    response = { temperature: nil }
     begin
       data = OpenWeatherConfig.client.current_zip(@zipcode, @country)
       response[:temperature] = {
@@ -49,22 +48,23 @@ module CurrentTemperatureService
                       when Faraday::TooManyRequestsError
                         "Too Many Requests - Key quota exceeded"
                       end
-      response[:error] = "#{error_message}. Status: #{status_code}"
-      response[:status] = e.response[:body]['cod'] || 503
+      status = e.response[:body]['cod'] || 503
+      error_msg = "#{error_message}. Status: #{status}"
     rescue Faraday::ConnectionFailed => e
       puts "Handling connection failure: #{e.response}"
-      response[:error] = 'Connection Failed'
-      response[:status] = e.response[:body]['cod'] || 503
+      error_msg = 'Connection Failed'
+      status = e.response[:body]['cod'] || 503
     rescue StandardError => e
       # TODO: Invalid api key is also giving in standard error
       puts "Handling standard error: #{e.response}"
-      response[:error] = e.message
-      response[:status] = e.response[:body]['cod'] || 503
+      error_msg = e.message
+      status = e.response[:body]['cod'] || 503
     ensure
+      response[:error] = error_msg
+      response[:status] = status
       puts "Response after external api is #{response}"
       return response  
     end
-    
   end    
 end
   
